fc0dfcb566c6653f27d67a3a91c53105933f8cb3
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-09-06 13:35:58 -0300

    Styled

--- App.js
+++ App.js
@@ -1,36 +1,23 @@
 import React, { Component } from "react";
-import { SafeAreaView, Text, View, Button } from 'react-native'
+import { Text, View, StyleSheet} from 'react-native'
 
 class App extends Component{
 
-  constructor(props){
-
-    super(props)
-
-    this.state = {
-      nome: 'Carlos'
-    }
-
-    this.entrar = this.entrar.bind(this)
-
-  }
-
-  entrar(nome){
-    this.setState({
-      nome
-    })
-  }
-
   render(){    
     return(
-      <SafeAreaView style={{ margin:20 }}>
-        <View style={{ margin:20 }}>
-
-          <Button title='Entrar' onPress={() => this.entrar('Matheus')}/>
-          <Text style={{color: 'yellow', fontSize: 50, textAlign: 'center'}}>{this.state.nome}</Text>
+        <View style={styles.area}>
+          <Text>I am text 1</Text>
+          <Text>I am text 2</Text>
+          <Text>I am text 3</Text>
+          <Text>I am text 4</Text>
         </View>
-      </SafeAreaView>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  area: {
+    margin: 50
+  }
+})
 export default App
